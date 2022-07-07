06782e24d00690705b6b8c296533ae17c90b9a36
YiWenTing <YiWenTing@jidian.com>
2022-07-07 11:05:17 +0800

    second commit

--- src/main/java/compojo/GitTest.java
+++ src/main/java/compojo/GitTest.java
@@ -3,5 +3,6 @@ package compojo;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git1!");
+        System.out.println("hello git2!");
     }
 }
